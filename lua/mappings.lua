6b354dfb4913a51588fde7ce2bb4cf9304305b1e
Brad Murdoch <Bmudrock@users.noreply.github.com>
2025-05-13 07:30:57 -0600

    Update mappings.lua
    nomap for terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,14 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+local nomap = vim.keymap.del
+
+nomap("n", "<leader>v")
+nomap("n", "<A-v>")
+nomap("t", "<A-v>")
+nomap("n", "<A-h>")
+nomap("t", "<A-h>")
+nomap("n", "<A-i>")
+nomap("t", "<A-i>")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
