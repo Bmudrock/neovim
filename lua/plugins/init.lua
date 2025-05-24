92c7a633929fd10eda732bac2572816d193fe75a
Brad Murdoch <Bmudrock@users.noreply.github.com>
2025-05-24 13:35:47 -0600

    added codecompanion plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,7 +12,14 @@ return {
       require "configs.lspconfig"
     end,
   },
-
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
